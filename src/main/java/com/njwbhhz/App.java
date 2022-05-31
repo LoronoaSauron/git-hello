4992983fefd1bdd65fd9f5cd0dfb165b1a130b7d
xiapguaishou <yaoyao@168.com>
2022-05-31 21:55:52 +0800

    提交你好hello-two

--- src/main/java/com/njwbhhz/App.java
+++ src/main/java/com/njwbhhz/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "test git集成!" );
         System.out.println( "分支完美运行!" );
+        System.out.println( "你好 hello-two!" );
     }
 }
